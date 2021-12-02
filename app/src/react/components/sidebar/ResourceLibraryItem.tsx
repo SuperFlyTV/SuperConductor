0961dea36406aee6bc982220ef320ab14479b5b1
Jan Starzak <jan.starzak@gmail.com>
2021-12-02 17:36:58 +0100

    chore: refactor styling

--- app/src/react/components/sidebar/ResourceLibraryItem.tsx
+++ app/src/react/components/sidebar/ResourceLibraryItem.tsx
@@ -3,30 +3,33 @@ import classNames from 'classnames'
 import { ResourceAny } from '@/models/resource/resource'
 import { useDrag } from 'react-dnd'
 import { ItemTypes } from '@/react/api/ItemTypes'
+import { describeResource } from '@/lib/Resources'
 
 type IProps = React.PropsWithChildren<{ resource: ResourceAny; selected: boolean; onClick?: React.MouseEventHandler }>
 
 export const ResourecLibraryItem = function ResourecLibraryItem({ resource, selected, onClick, children }: IProps) {
-	const [{ opacity }, dragRef] = useDrag(
+	const [{ dragged }, dragRef] = useDrag(
 		() => ({
 			type: ItemTypes.RESOURCE_ITEM,
 			item: { resource },
 			collect: (monitor) => ({
-				opacity: monitor.isDragging() ? 0.5 : 1,
+				dragged: monitor.isDragging() ? true : false,
 			}),
 		}),
 		[]
 	)
 
+	const resourceType = describeResource(resource)
+
 	return (
 		<div
 			ref={dragRef}
 			key={resource.id}
-			className={classNames('resource', 'resource-media', {
+			className={classNames('resource', resourceType, {
 				selected,
+				dragged,
 			})}
 			onClick={onClick}
-			style={{ opacity }}
 		>
 			{children}
 		</div>
