756d9ddebffb4f54877636d54d597ed272df1480
Alex Van Camp <vancamp.alex@gmail.com>
2022-01-31 07:57:13 -0600

    feat: show warning dialog when there is a version mismatch between SuperConductor and tsr-bridge

--- apps/app/src/electron/TimedPlayerThingy.ts
+++ apps/app/src/electron/TimedPlayerThingy.ts
@@ -1,4 +1,4 @@
-import { BrowserWindow, ipcMain } from 'electron'
+import { BrowserWindow, dialog, ipcMain } from 'electron'
 import { Group } from '../models/rundown/Group'
 import { IPCServer } from './IPCServer'
 import { IPCClient } from './IPCClient'
@@ -22,7 +22,7 @@ export class TimedPlayerThingy {
 
 	session: SessionHandler
 	storage: StorageHandler
-	bridgeHandler: BridgeHandler
+	bridgeHandler?: BridgeHandler
 
 	constructor() {
 		this.session = new SessionHandler()
@@ -32,22 +32,6 @@ export class TimedPlayerThingy {
 			x: undefined,
 			width: 1200,
 			height: 600,
-		})
-		this.bridgeHandler = new BridgeHandler(this.session, this.storage, {
-			updatedResources: (deviceId: string, resources: ResourceAny[]): void => {
-				// Added/Updated:
-				const newResouceIds = new Set<string>()
-				for (const resource of resources) {
-					newResouceIds.add(resource.id)
-					if (!_.isEqual(this.session.getResource(resource.id), resource)) {
-						this.session.updateResource(resource.id, resource)
-					}
-				}
-				// Removed:
-				for (const id of this.session.getResourceIds(deviceId)) {
-					if (!newResouceIds.has(id)) this.session.updateResource(id, null)
-				}
-			},
 		})
 
 		this.session.on('resource', (id: string, resource: ResourceAny | null) => {
@@ -70,14 +54,44 @@ export class TimedPlayerThingy {
 	initWindow(mainWindow: BrowserWindow) {
 		this.mainWindow = mainWindow
 
+		const bridgeHandler = new BridgeHandler(this.session, this.storage, {
+			updatedResources: (deviceId: string, resources: ResourceAny[]): void => {
+				// Added/Updated:
+				const newResouceIds = new Set<string>()
+				for (const resource of resources) {
+					newResouceIds.add(resource.id)
+					if (!_.isEqual(this.session.getResource(resource.id), resource)) {
+						this.session.updateResource(resource.id, resource)
+					}
+				}
+				// Removed:
+				for (const id of this.session.getResourceIds(deviceId)) {
+					if (!newResouceIds.has(id)) this.session.updateResource(id, null)
+				}
+			},
+			onVersionMismatch: (bridgeId: string, bridgeVersion: string, ourVersion: string): void => {
+				dialog
+					.showMessageBox(mainWindow, {
+						type: 'warning',
+						title: 'Bridge version mismatch',
+						message: `The connected bridge "${bridgeId}" is of a different version than SuperConductor.
+						\nBridge version: v${bridgeVersion}
+						SuperConductor version: v${ourVersion}
+						\nThis is likely to result in errors and unexpected behavior. Please ensure that both SuperConductor and tsr-bridge are up-to-date.`,
+					})
+					.catch(console.error)
+			},
+		})
+		this.bridgeHandler = bridgeHandler
+
 		this.ipcServer = new IPCServer(ipcMain, this.storage, this.session, {
 			refreshResources: () => {
 				// this.tptCaspar?.fetchAndSetMedia()
 				// this.tptCaspar?.fetchAndSetTemplates()
-				this.bridgeHandler.refreshResources()
+				bridgeHandler.refreshResources()
 			},
 			updateTimeline: (cache: UpdateTimelineCache, group: Group): GroupPreparedPlayheadData | null => {
-				return updateTimeline(cache, this.storage, this.bridgeHandler, group)
+				return updateTimeline(cache, this.storage, bridgeHandler, group)
 			},
 		})
 		this.ipcClient = new IPCClient(this.mainWindow)
--- apps/app/src/electron/bridgeHandler.ts
+++ apps/app/src/electron/bridgeHandler.ts
@@ -9,6 +9,8 @@ import _ from 'lodash'
 import { Mappings, TSRTimeline } from 'timeline-state-resolver-types'
 import { ResourceAny } from '@shared/models'
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { version: CURRENT_VERSION }: { version: string } = require('../../package.json')
 const SERVER_PORT = 5400
 
 /** This handles connected bridges */
@@ -31,6 +33,7 @@ export class BridgeHandler {
 		private storage: StorageHandler,
 		private callbacks: {
 			updatedResources: (deviceId: string, resources: ResourceAny[]) => void
+			onVersionMismatch: (bridgeId: string, bridgeVersion: string, ourVersion: string) => void
 		}
 	) {
 		this.server = new WebsocketServer(SERVER_PORT, (connection: WebsocketConnection) => {
@@ -39,6 +42,9 @@ export class BridgeHandler {
 			const bridge = new BridgeConnection(this, this.session, this.storage, connection, {
 				updatedResources: (deviceId: string, resources: ResourceAny[]) => {
 					this.callbacks.updatedResources(deviceId, resources)
+				},
+				onVersionMismatch: (bridgeId: string, bridgeVersion: string, ourVersion: string) => {
+					this.callbacks.onVersionMismatch(bridgeId, bridgeVersion, ourVersion)
 				},
 			})
 
@@ -160,6 +166,7 @@ export class BridgeConnection {
 		private connection: WebsocketConnection,
 		private callbacks: {
 			updatedResources: (deviceId: string, resources: ResourceAny[]) => void
+			onVersionMismatch: (bridgeId: string, bridgeVersion: string, ourVersion: string) => void
 		}
 	) {
 		const setConnected = () => {
@@ -209,7 +216,7 @@ export class BridgeConnection {
 			if (msg.type === 'initRequestId') {
 				this.onInitRequestId()
 			} else if (msg.type === 'init') {
-				this.onInit(msg.id)
+				this.onInit(msg.id, msg.version)
 			} else if (msg.type === 'status') {
 				// todo
 			} else if (msg.type === 'deviceStatus') {
@@ -271,11 +278,15 @@ export class BridgeConnection {
 		}
 	}
 
-	private onInit(id: string) {
+	private onInit(id: string, version: string) {
 		if (!this.bridgeId) {
 			this.bridgeId = id
 		} else if (this.bridgeId !== id) {
 			throw new Error(`bridgeId ID mismatch: "${this.bridgeId}" vs "${id}"`)
+		}
+
+		if (version !== CURRENT_VERSION) {
+			this.callbacks.onVersionMismatch(id, version, CURRENT_VERSION)
 		}
 
 		// Send initial commands:
