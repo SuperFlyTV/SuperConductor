60368d05acf68b9743c792c78b8f834a806147e5
Alex Van Camp <vancamp.alex@gmail.com>
2022-03-16 13:41:23 -0500

    Revert "fix: pair resource IDs with device IDs"
    This reverts commit e7f5b880441d79524fa49673aeaf4f17d77e37b4.

--- apps/app/src/electron/TimedPlayerThingy.ts
+++ apps/app/src/electron/TimedPlayerThingy.ts
@@ -71,22 +71,16 @@ export class TimedPlayerThingy {
 		const bridgeHandler = new BridgeHandler(this.session, this.storage, {
 			updatedResources: (deviceId: string, resources: ResourceAny[]): void => {
 				// Added/Updated:
-				const newResourceIds = new Set<string>()
+				const newResouceIds = new Set<string>()
 				for (const resource of resources) {
-					/**
-					 * Pairs the resourceId with the deviceId to help ensure uniqueness,
-					 * because resourceIds are not globally unique -- they're only
-					 * unique to their corresponding device.
-					 */
-					const compoundId = `${deviceId}_${resource.id}`
-					newResourceIds.add(compoundId)
-					if (!_.isEqual(this.session.getResource(compoundId), resource)) {
-						this.session.updateResource(compoundId, resource)
+					newResouceIds.add(resource.id)
+					if (!_.isEqual(this.session.getResource(resource.id), resource)) {
+						this.session.updateResource(resource.id, resource)
 					}
 				}
 				// Removed:
 				for (const id of this.session.getResourceIds(deviceId)) {
-					if (!newResourceIds.has(id)) this.session.updateResource(id, null)
+					if (!newResouceIds.has(id)) this.session.updateResource(id, null)
 				}
 			},
 			onVersionMismatch: (bridgeId: string, bridgeVersion: string, ourVersion: string): void => {
