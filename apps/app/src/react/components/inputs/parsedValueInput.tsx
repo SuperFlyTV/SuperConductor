508fbcc339e16509d37470e7a7868d7dc07ea77f
Jan Starzak <jan.starzak@gmail.com>
2022-03-24 08:38:30 +0100

    fix(parsedValueInput): persist temporary value on component unmount
    When the component is unmounting, check if the temporary value is valid,
    and if it is, send it upwards as a change.

--- apps/app/src/react/components/inputs/parsedValueInput.tsx
+++ apps/app/src/react/components/inputs/parsedValueInput.tsx
@@ -1,5 +1,5 @@
 import { TextField } from '@mui/material'
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
 export function ParsedValueInput<V>(
 	currentValue: V,
@@ -12,8 +12,10 @@ export function ParsedValueInput<V>(
 	disabled?: boolean,
 	fullWidth?: boolean
 ): JSX.Element {
-	const [value, setValue] = React.useState<string>('')
-	React.useEffect(() => {
+	const [value, setValue] = useState<string>('')
+	const fieldRef = useRef<HTMLInputElement>(null)
+
+	useEffect(() => {
 		setValue(stringify(currentValue))
 	}, [currentValue, stringify])
 
@@ -37,9 +39,22 @@ export function ParsedValueInput<V>(
 		setValue(str)
 	}
 
+	useEffect(() => {
+		const input = fieldRef.current
+		return () => {
+			if (input && input.value) {
+				const value = parse(input.value)
+				if (value !== undefined && value !== currentValue) {
+					onChange(value)
+				}
+			}
+		}
+	}, [fieldRef, currentValue, parse, onChange])
+
 	return (
 		<TextField
 			type={inputType}
+			inputRef={fieldRef}
 			onBlur={(e) => {
 				onSave(e.target.value)
 			}}
