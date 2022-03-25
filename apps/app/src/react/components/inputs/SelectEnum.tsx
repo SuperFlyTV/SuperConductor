5739a8e66955e334e32d8be1b66eebe6b6e26c37
Alex Van Camp <vancamp.alex@gmail.com>
2022-03-25 08:57:48 -0500

    feat(SelectEnum): add an optional confirmation dialog

--- apps/app/src/react/components/inputs/SelectEnum.tsx
+++ apps/app/src/react/components/inputs/SelectEnum.tsx
@@ -1,16 +1,46 @@
-import { MenuItem, TextField } from '@mui/material'
-import React from 'react'
+import { MenuItem, SxProps, TextField, Theme } from '@mui/material'
+import React, { useState } from 'react'
+import { ConfirmationDialog } from '../util/ConfirmationDialog'
 
-export const SelectEnum: React.FC<{
+type ConfirmationDialogProps =
+	| {
+			confirm?: false
+	  }
+	| {
+			confirm: true
+			confirmationTitle: string
+			confirmationMessage: string | ((proposedOption: Option) => string)
+			confirmationButtonLabel?: string
+			onConfirmationAccept?: (proposedOption: Option) => void
+			onConfirmationReject?: () => void
+	  }
+
+type SelectEnumProps = {
 	label: string
 	currentValue: any
 	options: { [key: string]: any }
 	onChange: (newValue: any) => void
+	sx?: SxProps<Theme>
 	allowUndefined?: boolean
 	defaultValue?: any
 	fullWidth?: boolean
-}> = ({ currentValue, options, onChange, allowUndefined, defaultValue, label, fullWidth }) => {
-	const allOptions: { [key: string]: { value: string | number; label: string } } = {}
+} & ConfirmationDialogProps
+
+type Option = { value: string | number; label: string }
+
+export const SelectEnum: React.FC<SelectEnumProps> = ({
+	currentValue,
+	options,
+	onChange,
+	allowUndefined,
+	defaultValue,
+	label,
+	fullWidth,
+	...props
+}) => {
+	const [confirmationDialogOpen, setConfirmationDialogOpen] = useState(false)
+	const [proposedOption, setProposedOption] = useState<Option>()
+	const allOptions: { [key: string]: Option } = {}
 
 	// Convert Typescript-enum to key-values:
 
@@ -51,30 +81,66 @@ export const SelectEnum: React.FC<{
 	}
 
 	return (
-		<TextField
-			select
-			margin="dense"
-			size="small"
-			fullWidth={fullWidth}
-			label={label}
-			value={currentValue}
-			onChange={(e) => {
-				if (allowUndefined && e.target.value === '__undefined') {
-					onChange(undefined)
-				} else {
-					const selectedOption = allOptions[e.target.value]
-					if (selectedOption) onChange(selectedOption.value)
-					else throw new Error('Unknown option: ' + e.target.value)
-				}
-			}}
-		>
-			{Object.entries(allOptions).map(([key, value]) => {
-				return (
-					<MenuItem key={key} value={key}>
-						{value.label}
-					</MenuItem>
-				)
-			})}
-		</TextField>
+		<>
+			<TextField
+				select
+				margin="dense"
+				size="small"
+				fullWidth={fullWidth}
+				label={label}
+				value={currentValue}
+				sx={props.sx}
+				onChange={(e) => {
+					if (props.confirm) {
+						setProposedOption(allOptions[e.target.value])
+						setConfirmationDialogOpen(true)
+					} else if (allowUndefined && e.target.value === '__undefined') {
+						onChange(undefined)
+					} else {
+						const selectedOption = allOptions[e.target.value]
+						if (selectedOption) onChange(selectedOption.value)
+						else throw new Error('Unknown option: ' + e.target.value)
+					}
+				}}
+			>
+				{Object.entries(allOptions).map(([key, value]) => {
+					return (
+						<MenuItem key={key} value={key}>
+							{value.label}
+						</MenuItem>
+					)
+				})}
+			</TextField>
+
+			{props.confirm && (
+				<ConfirmationDialog
+					open={confirmationDialogOpen}
+					title={props.confirmationTitle}
+					body={
+						typeof props.confirmationMessage === 'string'
+							? props.confirmationMessage
+							: typeof proposedOption === 'undefined'
+							? 'Are you sure you wish to commit this change?'
+							: props.confirmationMessage(proposedOption)
+					}
+					acceptLabel={props.confirmationButtonLabel ?? 'Confirm'}
+					onAccepted={() => {
+						if (proposedOption) {
+							onChange(proposedOption.value)
+							if (props.onConfirmationAccept) {
+								props.onConfirmationAccept(proposedOption)
+							}
+						}
+						setConfirmationDialogOpen(false)
+					}}
+					onDiscarded={() => {
+						if (props.onConfirmationReject) {
+							props.onConfirmationReject()
+						}
+						setConfirmationDialogOpen(false)
+					}}
+				/>
+			)}
+		</>
 	)
 }
