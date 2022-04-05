70ccbeae79601bd90334a5ade225fcd0630a77e2
Johan Nyman <johan@nytamin.se>
2022-04-05 10:11:56 +0200

    chore: less aaa and more wow

--- apps/app/src/react/mobx/lib.ts
+++ apps/app/src/react/mobx/lib.ts
@@ -5,7 +5,6 @@ import { useMemo } from 'react'
 /** Variant of useMemo, useful when memoizing objects computed from a mobx store */
 export function useMemoComputedObject<T extends object>(fcn: () => T, deps: React.DependencyList): T {
 	return useMemo(() => {
-		console.log('aaa')
 		return computed<T>(fcn, {
 			equals: _.isEqual,
 		})
