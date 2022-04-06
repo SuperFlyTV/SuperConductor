73d0aa48712d16a62c41bf0d3bf6668957daabdd
Johan Nyman <johan@nytamin.se>
2022-04-06 14:50:04 +0200

    chore: add description to Layers settings

--- apps/app/src/react/components/pages/homePage/MappingsPage.tsx
+++ apps/app/src/react/components/pages/homePage/MappingsPage.tsx
@@ -1,29 +1,23 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { observer } from 'mobx-react-lite'
-import React, { useContext } from 'react'
-import { TrashBtn } from '../../inputs/TrashBtn'
-import { store } from '../../../mobx/store'
-import { Button } from '@mui/material'
-import { IPCServerContext } from '../../../contexts/IPCServer'
-import { ErrorHandlerContext } from '../../../contexts/ErrorHandler'
+import React from 'react'
 
 import { ProjectPageLayout } from './projectPageLayout/ProjectPageLayout'
 import { Project } from 'src/models/project/Project'
 import { MappingSettings } from '../../settings/mappings/MappingSettings'
 
 export const MappingsPage: React.FC<{ project: Project }> = observer(function MappingsPage(props) {
-	const serverAPI = useContext(IPCServerContext)
-	const { handleError } = useContext(ErrorHandlerContext)
-	const rundownsStore = store.rundownsStore
-	const guiStore = store.guiStore
-
-	const handleReopen = (rundownId: string) => {
-		serverAPI.openRundown({ rundownId }).catch(handleError)
-		// guiStore.currentlyActiveTabSection = 'rundown'
-	}
+	const help = (
+		<>
+			Layers contain the settings for <i>where</i> things are to be played out.
+			<br />
+			Make sure you&apos;ve added the device you want to control on the Bridges page. Then add a layer, pick that
+			device and set the settings.
+		</>
+	)
 
 	return (
-		<ProjectPageLayout title="Layers">
+		<ProjectPageLayout title="Layers" help={help}>
 			<MappingSettings project={props.project} />
 		</ProjectPageLayout>
 	)
