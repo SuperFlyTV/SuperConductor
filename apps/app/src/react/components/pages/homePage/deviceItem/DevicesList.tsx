4f06cf66dbcb69a8a50faaf0f1f73d8692c8296d
Johan Nyman <johan@nytamin.se>
2022-06-07 10:05:27 +0200

    fix: devices didn't show up in list if the Bridge was disconnected

--- apps/app/src/react/components/pages/homePage/deviceItem/DevicesList.tsx
+++ apps/app/src/react/components/pages/homePage/deviceItem/DevicesList.tsx
@@ -1,11 +1,12 @@
 import { observer } from 'mobx-react-lite'
 import React from 'react'
 import { Project } from '../../../../../models/project/Project'
-import { Bridge, BridgeStatus } from '../../../../../models/project/Bridge'
+import { Bridge, BridgeDevice, BridgeStatus } from '../../../../../models/project/Bridge'
 import { ScList } from '../scList/ScList'
 import { DeviceItemContent } from './DeviceItemContent'
 import { DeviceItemHeader } from './DeviceItemHeader'
 import { getDeviceName } from '../../../../../lib/util'
+import { literal } from '@shared/lib'
 
 export const DevicesList: React.FC<{
 	bridge: Bridge
@@ -13,11 +14,30 @@ export const DevicesList: React.FC<{
 	project: Project
 	newlyCreatedDeviceId: string | undefined
 }> = observer(function DevicesList(props) {
+	const devices: BridgeStatus['devices'] = {}
+	for (const deviceId of Object.keys(props.bridge.settings.devices)) {
+		const status = props.devices[deviceId] as BridgeDevice | undefined
+
+		if (status) {
+			devices[deviceId] = status
+		} else {
+			devices[deviceId] = literal<BridgeDevice>({
+				connectionId: 0,
+				ok: false,
+				message: 'Not connected',
+			})
+		}
+	}
+
+	if (Object.keys(devices).length === 0) {
+		return <div className="central">There are no devices.</div>
+	}
+
 	return (
 		<div className="devices-list">
 			<ScList
 				openByDefault={props.newlyCreatedDeviceId ? [props.newlyCreatedDeviceId] : []}
-				list={Object.entries(props.devices)
+				list={Object.entries(devices)
 					/**
 					 * Temporary fix - required because props.devices and props.bridge.settings.device
 					 * do not have the same devices (props.devices does not get updated).
