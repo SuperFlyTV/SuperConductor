58e1fdac9ee9f588d877cb0846e8c29f4b0ec976
Johan Nyman <johan@nytamin.se>
2022-09-15 13:21:47 +0200

    fix: add API endpoints at "/" and "/api", listing the available methods

--- apps/app/src/electron/HTTPAPI.ts
+++ apps/app/src/electron/HTTPAPI.ts
@@ -9,65 +9,72 @@ export class HTTPAPI {
 	private app = new Koa()
 	private router = new Router()
 
+	private methodSignatures: {
+		[fullEndpoint: string]: {
+			endpoint: string
+			type: string
+		}
+	} = {}
+
 	constructor(port: number, ipcServer: IPCServer) {
+		this.router.get(`/`, async (ctx) => {
+			ctx.response.body = `<html><body>
+			<a href="/api">API</a>
+			</body></html>`
+			ctx.response.status = 200
+		})
+		this.router.get(`/api`, async (ctx) => {
+			const methods = Object.entries(this.methodSignatures)
+				.map(([_fullEndpoint, e]) => {
+					const url = `/api/${e.endpoint}/?`
+
+					return `<a href="${url}">${e.type} ${url}</a>`
+				})
+				.join('<br>\n')
+
+			ctx.response.body = `<html><body>${methods}</body></html>`
+			ctx.response.status = 200
+		})
+
 		for (const methodName of Object.getOwnPropertyNames(IPCServer.prototype)) {
 			// Ignore "private" methods.
-			if (methodName[0] === '_') {
-				continue
-			}
+			if (methodName[0] === '_') continue
+			if (methodName === 'constructor') continue
 
-			const fcn = (ipcServer as any)[methodName].bind(ipcServer)
-
+			const originalMethod = (ipcServer as any)[methodName]
 			// Ignore methods that don't exist.
-			if (!fcn) {
-				continue
-			}
+			if (!originalMethod) continue
+
+			const fcn = originalMethod.bind(ipcServer)
+
+			let endpoint: string
+			let endpointType: 'GET' | 'POST' | 'DELETE'
 
 			if (methodName.startsWith('get')) {
 				// Handle GET requests
-				this.router.get(
-					`/api/${methodName.charAt(3).toLocaleLowerCase() + methodName.substring(4)}`,
-					async (ctx) => {
-						try {
-							const result = await fcn(ctx.request.query)
-							ctx.response.body = result
-							ctx.response.status = 200
-						} catch (error) {
-							const stringifiedError = stringifyError(error)
-							if (stringifiedError.includes('not found')) {
-								ctx.response.status = 404
-							} else {
-								ctx.response.status = 500
-							}
+
+				endpoint = methodName.charAt(3).toLocaleLowerCase() + methodName.substring(4)
+				endpointType = 'GET'
+				this.router.get(`/api/${endpoint}`, async (ctx) => {
+					try {
+						const result = await fcn(ctx.request.query)
+						ctx.response.body = result
+						ctx.response.status = 200
+					} catch (error) {
+						const stringifiedError = stringifyError(error)
+						if (stringifiedError.includes('not found')) {
+							ctx.response.status = 404
+						} else {
+							ctx.response.status = 500
 						}
 					}
-				)
+				})
 			} else if (methodName.startsWith('delete')) {
 				// Handle DELETE requests
-				this.router.delete(
-					`/api/${methodName.charAt(6).toLocaleLowerCase() + methodName.substring(7)}`,
-					async (ctx) => {
-						try {
-							const result = await fcn(ctx.request.query)
-							if (isUndoable(result)) {
-								ctx.response.body = result.result
-							} else {
-								ctx.response.body = result
-							}
-							ctx.response.status = 200
-						} catch (error) {
-							const stringifiedError = stringifyError(error)
-							if (stringifiedError.includes('not found')) {
-								ctx.response.status = 404
-							} else {
-								ctx.response.status = 500
-							}
-						}
-					}
-				)
-			} else {
-				// Handle POST requests
-				this.router.post(`/api/${methodName}`, async (ctx) => {
+
+				endpoint = methodName.charAt(6).toLocaleLowerCase() + methodName.substring(7)
+				endpointType = 'DELETE'
+				this.router.delete(`/api/${endpoint}`, async (ctx) => {
 					try {
 						const result = await fcn(ctx.request.query)
 						if (isUndoable(result)) {
@@ -85,6 +92,39 @@ export class HTTPAPI {
 						}
 					}
 				})
+			} else {
+				// Handle POST requests
+
+				endpoint = methodName
+				endpointType = 'POST'
+				this.router.post(`/api/${endpoint}`, async (ctx) => {
+					try {
+						const result = await fcn(ctx.request.query)
+						if (isUndoable(result)) {
+							ctx.response.body = result.result
+						} else {
+							ctx.response.body = result
+						}
+						ctx.response.status = 200
+					} catch (error) {
+						const stringifiedError = stringifyError(error)
+						if (stringifiedError.includes('not found')) {
+							ctx.response.status = 404
+						} else {
+							ctx.response.status = 500
+						}
+					}
+				})
+			}
+
+			const fullEndpoint = `${endpointType} ${endpoint}`
+			if (this.methodSignatures[fullEndpoint]) {
+				throw new Error(`Dupliacte API endpoints "${fullEndpoint}"!`)
+			}
+			this.methodSignatures[fullEndpoint] = {
+				type: endpointType,
+				endpoint,
+				// TODO: how to extract the signature of the function here?
 			}
 		}
 
