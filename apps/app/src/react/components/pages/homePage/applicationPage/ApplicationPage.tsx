fe908750404bca61c09a8ef446f2004beb1b2a24
Johan Nyman <johan@nytamin.se>
2022-11-28 17:44:49 +0100

    chore: GUI: add help message for auto-update

--- apps/app/src/react/components/pages/homePage/applicationPage/ApplicationPage.tsx
+++ apps/app/src/react/components/pages/homePage/applicationPage/ApplicationPage.tsx
@@ -1,5 +1,5 @@
 import { observer } from 'mobx-react-lite'
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { IPCServerContext } from '../../../../contexts/IPCServer'
 import { ErrorHandlerContext } from '../../../../contexts/ErrorHandler'
 import { ProjectPageLayout } from '../projectPageLayout/ProjectPageLayout'
@@ -8,6 +8,8 @@ import Toggle from 'react-toggle'
 
 import './style.scss'
 import { computed } from 'mobx'
+import { HelpButton } from '../../../inputs/HelpButton/HelpButton'
+import { Message } from '../message/Message'
 
 export const ApplicationPage: React.FC = observer(function ApplicationPage() {
 	const serverAPI = useContext(IPCServerContext)
@@ -15,11 +17,30 @@ export const ApplicationPage: React.FC = observer(function ApplicationPage() {
 
 	const preReleaseAutoUpdate = computed(() => store.appStore?.appData?.preReleaseAutoUpdate ?? false)
 
+	const [showHelp, setShowHelp] = useState(false)
+
 	return (
 		<ProjectPageLayout title={'Application settings'} subtitle="">
 			<div>
 				<div>
-					<label>Auto-update to pre-release (alpha) versions</label>
+					<label>
+						Auto-update to pre-release (alpha) versions
+						<HelpButton
+							showHelp={showHelp}
+							onClick={() => {
+								setShowHelp(!showHelp)
+							}}
+						/>
+					</label>
+
+					{showHelp && (
+						<Message type="help">
+							If you turn on this, SuperConductor will auto-update to the latest pre-release version.
+							<br />
+							The pre-release versions are released more frequently and have the latest features, but have
+							a higher risk of bugs.
+						</Message>
+					)}
 					<div className="sc-switch">
 						<Toggle
 							checked={preReleaseAutoUpdate.get()}
