dbc760e0e576ee638048625ebb4f1950b3773bbe
Johan Nyman <johan@nytamin.se>
2023-10-20 15:56:30 +0200

    chore: add test

--- apps/app/src/lib/__tests__/util.test.ts
+++ apps/app/src/lib/__tests__/util.test.ts
@@ -19,4 +19,69 @@ test('deepExtendRemovingUndefined', () => {
 		})
 		expect(target).toStrictEqual({ a: [4, 5, 6] })
 	}
+
+	{
+		const target = {
+			arr: [1, 2, 3],
+			obj: {
+				num: 1,
+				arr: [
+					{
+						a: 1,
+					},
+					{
+						b: 2,
+					},
+				],
+				obj: {
+					a: 1,
+				},
+				toBeUndefined: {
+					a: 1,
+				},
+			},
+		}
+		deepExtendRemovingUndefined(target, {
+			arr: [3, 4, 5],
+			obj: {
+				num: 2,
+				arr: [
+					{
+						a: 2,
+					},
+					{
+						b: 3,
+					},
+					{
+						c: 4,
+					},
+				],
+				obj: {
+					b: 2,
+				},
+				toBeUndefined: undefined,
+			},
+		})
+		expect(target).toStrictEqual({
+			arr: [3, 4, 5],
+			obj: {
+				num: 2,
+				arr: [
+					{
+						a: 2,
+					},
+					{
+						b: 3,
+					},
+					{
+						c: 4,
+					},
+				],
+				obj: {
+					a: 1,
+					b: 2,
+				},
+			},
+		})
+	}
 })
