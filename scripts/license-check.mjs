d9e8db9a1b1a814564f3f44d6d8e7a9be7176aed
Julian Waller <git@julusian.co.uk>
2024-10-11 15:51:43 +0100

    chore: fix license check script

--- scripts/license-check.mjs
+++ scripts/license-check.mjs
@@ -1,8 +1,14 @@
 /* eslint-disable n/no-process-exit, n/no-extraneous-require,@typescript-eslint/no-require-imports */
-'use strict'
-const fs = require('fs')
-const path = require('path')
-const shell = require('shelljs')
+// @ts-check
+
+import fs from 'fs'
+import path from 'path'
+import checker from 'license-checker'
+import { createRequire } from 'module'
+
+const require = createRequire(import.meta.url)
+
+const DEBUG = false
 
 function getDirectories(source) {
 	return fs
@@ -14,20 +20,36 @@ function getDirectories(source) {
 const sharedPackages = getDirectories(path.resolve('./shared/packages'))
 if (sharedPackages.length < 4) throw new Error('expected more shared/packages')
 
+const ROOT_VERSION = require('../package.json').version
 const CURRENT_VERSION = require('@shared/api/package.json').version
 
 const allowPackages = [
-	`buffers@0.1.1`,
 	`caniuse-lite@1.0.30001465`,
-	`cycle@1.0.3`,
 	`truncate-utf8-bytes@1.0.2`,
 	`utf8-byte-length@1.0.4`,
 	...sharedPackages.map((pkg) => `@shared/${pkg}@${CURRENT_VERSION}`),
+	`superconductor-monorepo@${ROOT_VERSION}`,
 	`superconductor@${CURRENT_VERSION}`,
 	`tsr-bridge@${CURRENT_VERSION}`,
 ]
 
-const cmd = ['yarn', 'sofie-licensecheck', '--allowPackages', `"${allowPackages.join(';')}"`]
-
-const res = shell.exec(cmd.join(' '))
-process.exit(res.code)
+checker.init(
+	{
+		start: path.resolve('.'),
+		onlyAllow: 'MIT;BSD;ISC;Apache-2.0;CC0;CC-BY-3.0;CC-BY-4.0;Unlicense;Artistic-2.0;Python-2.0;BlueOak-1.0.0',
+		excludePackages: allowPackages.join(';'),
+		summary: !DEBUG,
+	},
+	(err, packages) => {
+		if (err) {
+			//Handle error
+			console.error(err)
+			process.exit(1)
+		} else {
+			if (DEBUG) {
+				console.log(packages)
+			}
+			process.exit(0)
+		}
+	}
+)
