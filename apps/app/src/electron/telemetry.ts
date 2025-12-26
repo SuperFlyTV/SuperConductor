564a9b6df8efba2bcadb4dae15b4ab09af331981
dedicatedbroadcastsolutions <zcybercomputing@gmail.com>
2025-12-26 15:36:04 +0000

    Fix Prettier indentation in CasparCG and telemetry

--- apps/app/src/electron/telemetry.ts
+++ apps/app/src/electron/telemetry.ts
@@ -136,7 +136,7 @@ export class TelemetryHandler {
 				try {
 					// The Node 'fetch' builtin is only supported in Node 21+; allow it here for runtime environments that provide fetch
 					// eslint-disable-next-line n/no-unsupported-features/node-builtins
-				const response = await fetch(
+					const response = await fetch(
 						// 'http://superconductor-statistics/superconductor/reportUsageStatistics',
 						// 'http://localhost:2500/superconductor/reportUsageStatistics',
 						// 'https://faas-ams3-2a2df116.doserverless.co/api/v1/web/fn-7ba9c1fc-f987-4993-b324-a86c98928fcb/telemetry/insert',
--- shared/packages/tsr-bridge/src/sideload/CasparCG.ts
+++ shared/packages/tsr-bridge/src/sideload/CasparCG.ts
@@ -120,30 +120,27 @@ export class CasparCGSideload implements SideLoadDevice {
 				// Use helper to parse framerate and calculate duration/frameTime
 				let duration = 0
 				let frameTime = ''
-			if (
-				(media as any).duration != null &&
-				typeof (media as any).duration === 'number' &&
-				(media as any).duration > 0
-			) {
-				duration = (media as any).duration
-			} else {
-				duration = durationFromFrames(media.frames, media.framerate)
-			}
+				if (
+					(media as any).duration != null &&
+					typeof (media as any).duration === 'number' &&
+					(media as any).duration > 0
+				) {
+					duration = (media as any).duration
+				} else {
+					duration = durationFromFrames(media.frames, media.framerate)
+				}
 
-			if (
-				media.frames != null &&
-				media.framerate != null &&
-				typeof media.frames === 'number' &&
-				typeof media.framerate === 'number'
-			) {
-				frameTime = frameTimeFromFrames(media.frames, media.framerate)
-			}
-const resource: CasparCGMedia = {
-				resourceType: ResourceType.CASPARCG_MEDIA,
-				deviceId: this.deviceId,
-					id: protectString(''), // set by getResourceIdFromResource() later
-					type,
-					name: media.clip,
+				if (
+					media.frames != null &&
+					media.framerate != null &&
+					typeof media.frames === 'number' &&
+					typeof media.framerate === 'number'
+				) {
+					frameTime = frameTimeFromFrames(media.frames, media.framerate)
+				}
+				const resource: CasparCGMedia = {
+					resourceType: ResourceType.CASPARCG_MEDIA,
+					deviceId: this.deviceId,
 					displayName: media.clip,
 					changed: media.datetime,
 					duration,
